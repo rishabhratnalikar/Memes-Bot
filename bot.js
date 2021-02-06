d0367e91bbd2a5a3cd8c4d9bdb54b2865c49313b
rishabhratnalikar <75599765+rishabhratnalikar@users.noreply.github.com>
2021-02-06 18:59:34 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,1 +1,17 @@
+const Discord = require("discord.js")
+const client = new Discord.Client()
 
+
+client.on('ready', () =>{
+  
+  console.log("!Meme bot is online")
+ 
+})
+client.on('message', msg=>{
+  
+  
+if(message.content == "Share some memes"){
+  msg.reply('https://tenor.com/view/hello-sexy-hi-hello-mr-bean-gif-13830351')
+}
+
+})
